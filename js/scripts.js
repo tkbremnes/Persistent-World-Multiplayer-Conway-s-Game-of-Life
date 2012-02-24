9af7fb229765d53bf198da98cf12bccf3a2cb9ab
Trond Kjetil Bremnes <tkbremnes@gmail.com>
2012-02-24 19:51:47 +0100

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+window.onload = init;
+
+var canvas;
+var ctx;
+
+function init(){
+	canvas = document.querySelector('canvas');
+	context= canvas.getContext('2d');
+}
+
+function drawGrid()
+{
+
+}
