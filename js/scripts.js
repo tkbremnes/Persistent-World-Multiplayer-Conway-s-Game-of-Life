124f320a1fe21ca8b38c245dbb77150a718fb9f2
Trond Kjetil Bremnes <tkbremnes@gmail.com>
2012-02-24 21:26:58 +0100

    the game works

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,155 @@ window.onload = init;
 
 var canvas;
 var ctx;
+var canvasWidth;
+var canvasHeight;
+var cellSize;
+var board;
+var noOfCellsHorizontal;
+var noOfCellsVertical;
+var tickLength = 2;
 
 function init(){
 	canvas = document.querySelector('canvas');
-	context= canvas.getContext('2d');
+	canvasWidth = canvas.width;
+	canvasHeight = canvas.height;
+
+	// TODO
+	cellSize = 16;
+	noOfCellsHorizontal = Math.floor(canvasWidth/cellSize);
+	noOfCellsVertical = Math.floor(canvasHeight/cellSize);
+	
+	//initialize the empty board
+	board = new Array(noOfCellsHorizontal);
+	for(var i=0; i<=noOfCellsHorizontal-1; i++)
+	{
+		board[i] = new Array(noOfCellsVertical);
+
+		for(var j=0; j<=noOfCellsVertical-1; j++)
+		{
+			board[i][j] = 0;
+		}
+	}
+	console.log(board);
+
+	ctx = canvas.getContext('2d');
+
+	canvas.addEventListener('mouseup', registerInput, false);
+	drawBoard();
+	window.setInterval('gameTick()', tickLength*1000);
 }
 
 function drawGrid()
 {
+	ctx.strokeStyle = 'rgb(205,201,201)';
+	ctx.lineWidth = 1;
 
+	for(var i=0; i<=canvasWidth; i=i+cellSize){
+		ctx.beginPath();
+		ctx.moveTo(i,0);
+		ctx.lineTo(i,canvasHeight);
+		ctx.stroke();
+	}
+
+	for(var i=0; i<=canvasHeight; i=i+cellSize){
+		ctx.beginPath();
+		ctx.moveTo(0,i);
+		ctx.lineTo(canvasWidth,i);
+		ctx.stroke();
+	}
+}
+
+function registerInput(ev)
+{
+	var x, y;
+  	if (ev.offsetX || ev.offsetX == 0)
+  	{
+   		x = ev.offsetX;
+    	y = ev.offsetY;
+  	}
+  	console.log("x: " + x + ", y: " + y);
+
+  	doSomething(Math.floor(x/16), Math.floor(y/16));
+}
+
+function doSomething(x, y)
+{
+  	console.log("x: " + x + ", y: " + y);
+	board[x][y] = 1;
+	console.log(board[x][y]);
+	drawBoard();
+}
+
+function drawBoard()
+{
+	// redraw the frame
+	ctx.fillStyle = 'rgb(255,255,255)';
+	ctx.fillRect(0,0,canvasWidth, canvasHeight);
+
+	// draw the helper grid
+	drawGrid();
+
+	// Draws the board
+	console.log("drawing board");
+	ctx.fillStyle = 'rgb(0,0,0)';
+	for(var i=0; i<=noOfCellsHorizontal-1; i++)
+	{
+
+	for(var j=0; j<=noOfCellsVertical-1; j++)
+		{
+			if(board[i][j] == 1){
+				ctx.fillRect(i*cellSize,j*cellSize,cellSize,cellSize);
+			}
+		}
+	}
+}
+
+function gameTick()
+{
+	for(var i=0; i<=noOfCellsHorizontal-1; i++)
+	{
+		for(var j=0; j<=noOfCellsVertical-1; j++)
+		{
+			numberOfNeighbours = getNumberOfNeighbours(i,j);
+			if(board[i][j] == 1){
+				if(numberOfNeighbours==0)
+				{
+					console.log("no neighbours, dying...")
+					board[i][j] = 0;
+				}
+				else if(numberOfNeighbours>=4){
+					console.log("overpopulation, dying...")
+					board[i][j] = 0;
+				}
+			}
+			else
+			{
+				if(numberOfNeighbours == 2 ||numberOfNeighbours == 3)
+				{
+					board[i][j] = 1;
+				}
+			}
+		}
+	}
+	drawBoard();
+}
+
+function getNumberOfNeighbours(x,y)
+{
+	var result = 0;
+	for(var i=-1; i<=1; i++)
+	{
+		for(var j=-1; j<=1; j++)
+		{
+			if((i!=0 && j!=0) && (x+i>=0 && y+j>=0) && (x+i<=noOfCellsHorizontal-1 && y+j<=noOfCellsVertical-1))
+			{
+				if(board[x+i][y+j] == 1)
+				{
+					console.log("found neighbour at (" + x + ", " + y + ")")
+					result += 1;
+				}
+			}
+		}
+	}
+	return result;
 }
